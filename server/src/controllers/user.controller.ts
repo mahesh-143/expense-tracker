b5ac90ec1590aee25e8b790f7c725086d56ffa64
mahesh-143 <maheshodedara13@yahoo.com>
2024-09-22 20:08:28 +0530

    refactor(routes): separate HTTP-related logic from user controllers

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,83 +1,58 @@
-import { Request, Response, NextFunction } from "express";
 import { db } from "../drizzle/db";
 import { eq } from "drizzle-orm";
-import { UserTable } from "../drizzle/schema";
+import { User, UserTable } from "../drizzle/schema";
 import { findUserByEmail, findUserById } from "../utils/users";
+import { HttpError } from "../middlewares/HttpError";
 
-export const getUser = async (
-  req: Request<{ id: string }>,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { id } = req.params;
+export const getUser = async (id: string) => {
+  const userProfile = await findUserById(id);
 
-    const userProfile = await findUserById(id);
-
-    if (!userProfile) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    const { password: _, ...user } = userProfile;
-    res.status(200).json({ user });
-  } catch (error) {
-    next(error);
+  if (!userProfile) {
+    throw new HttpError("User not found", 404);
   }
+
+  const { password: _, ...user } = userProfile;
+  return user;
 };
 
-export const deleteUser = async (
-  req: Request & { body: { id: string } },
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { id } = req.body;
-    const userExists = await findUserById(id);
+export const deleteUser = async (id: string) => {
+  const userExists = await findUserById(id);
 
-    if (!userExists) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    const result = await db.delete(UserTable).where(eq(UserTable.id, id));
-    res.status(200).json({ result, message: "User deleted!" });
-  } catch (error) {
-    next(error);
+  if (!userExists) {
+    throw new HttpError("User not found", 404);
   }
+  await db.delete(UserTable).where(eq(UserTable.id, id));
+  return { message: "user deleted" };
 };
 
-export const updateUser = async (
-  req: Request & { body: { id: string; email: string; username: string } },
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { id, email, username } = req.body;
-    if (!email || !username) {
-      return res.status(400).json({ message: "Request body is empty" });
-    }
+export const updateUser = async (data: User, user_id: string) => {
+  const { id, email, username } = data;
 
-    const userExists = await findUserById(id);
-
-    if (!userExists) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    const existingEmailUser = await findUserByEmail(email);
-
-    if (existingEmailUser) {
-      return res.status(409).json({ message: "Email already in use" });
-    }
-    const [updatedUser] = await db
-      .update(UserTable)
-      .set({ email, username })
-      .where(eq(UserTable.id, id))
-      .returning({
-        id: UserTable.id,
-        username: UserTable.username,
-        email: UserTable.email,
-      });
-
-    res.status(200).json({ message: "User Updated!", updatedUser });
-  } catch (error) {
-    next(error);
+  if (!email || !username) {
+    throw new HttpError("Missing required feilds", 400);
   }
+
+  const userExists = await findUserById(user_id);
+
+  if (!userExists) {
+    throw new HttpError("User not found", 404);
+  }
+
+  const existingEmailUser = await findUserByEmail(email);
+
+  if (existingEmailUser) {
+    throw new HttpError("Email already in use", 409);
+  }
+
+  const [updatedUser] = await db
+    .update(UserTable)
+    .set({ email, username })
+    .where(eq(UserTable.id, id))
+    .returning({
+      id: UserTable.id,
+      username: UserTable.username,
+      email: UserTable.email,
+    });
+
+  return updatedUser;
 };
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,8 +1,37 @@
 import { Router } from "express";
 import * as userController from "../controllers/user.controller";
+import authenticateToken from "../middlewares/authenticateToken";
+import { asyncHandler } from "../utils/asyncHandler";
 const router = Router();
 
-router.put("/edit/:id", userController.updateUser);
-router.delete("/delete", userController.deleteUser);
-router.get("/:id", userController.getUser);
+router.get(
+  "/:id",
+  authenticateToken,
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const user = await userController.getUser(id);
+    res.status(200).json({ user });
+  }),
+);
+
+router.put(
+  "edit/:id",
+  authenticateToken,
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const updatedUser = await userController.updateUser(req.body, id);
+    res.status(200).json(updatedUser);
+  }),
+);
+
+router.delete(
+  "/delete/:id",
+  authenticateToken,
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const deleteUser = await userController.deleteUser(id);
+    res.status(200).json(deleteUser);
+  }),
+);
+
 export default router;
